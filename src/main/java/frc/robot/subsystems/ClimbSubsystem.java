f3a67ec8e5894d5c00d85c78790fa51c6a29bc94
FRC Team 5826 AvisAutomata <frc5826@gmail.com>
2026-04-09 17:10:24 -0500

    Climb is More Dead

--- src/main/java/frc/robot/subsystems/ClimbSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimbSubsystem.java
@@ -12,87 +12,86 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import frc.robot.commands.LoggedCommand;
 
-import static frc.robot.Constants.Climb.*;
 
 public class ClimbSubsystem extends LoggedSubsystem {
 
-    private SparkMax motor;
-    private double lastPos;
-
-    public ClimbSubsystem (){
-
-        motor = new SparkMax(cClimber, SparkLowLevel.MotorType.kBrushless);
-        SparkBaseConfig config = new SparkMaxConfig()
-                .apply(new EncoderConfig().positionConversionFactor(cConfigMultiplier))
-                .apply(new ClosedLoopConfig().pid(5, 0, 0).outputRange(-0.5,1));
-        motor.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
-        zeroClimb();
-        hookStow();
-        SmartDashboard.putData("5826/climb/zeroClimber", zeroCommand());
-    }
-
-    @Override
-    public void periodic() {
-        super.periodic();
-        SmartDashboard.putNumber("5826/climb/position", motor.getEncoder().getPosition());
-    }
-
-    public void hookDown() {
-        if(motor.getEncoder().getPosition()<=cDownPos*1.5){
-            motor.getClosedLoopController().setSetpoint(cDownPos, SparkBase.ControlType.kPosition, ClosedLoopSlot.kSlot0, 0);
-
-        }else
-            motor.getClosedLoopController().setSetpoint(cDownPos, SparkBase.ControlType.kPosition, ClosedLoopSlot.kSlot0, -6);
-    }
-
-    public void hookStow() {
-        if(motor.getEncoder().getPosition()<=cDownPos*1.5){
-            motor.getClosedLoopController().setSetpoint(cStowPos, SparkBase.ControlType.kPosition, ClosedLoopSlot.kSlot0, 0);
-        }else
-            motor.getClosedLoopController().setSetpoint(cStowPos, SparkBase.ControlType.kPosition, ClosedLoopSlot.kSlot0, -6);
-    }
-
-    public void hookUp() {
-        motor.getClosedLoopController().setSetpoint(cUpPos, SparkBase.ControlType.kPosition, ClosedLoopSlot.kSlot0);
-    }
-
-    public void zeroClimb () {
-        motor.getEncoder().setPosition(0);
-    }
-
-    public void motorDown () {
-        motor.set(-0.05);
-    }
-
-    public Command hookDownCommand() {
-        Command c = new RunCommand(this::hookDown,this);
-        return LoggedCommand.logCommand(c,"Climb Command");
-    }
-
-    public Command hookUpCommand() {
-        Command c = new RunCommand(this::hookUp, this);
-        return LoggedCommand.logCommand(c, "Up Command");
-    }
-
-    public Command stowCommand () {
-        Command c = new RunCommand(this::hookStow, this);
-        return LoggedCommand.logCommand(c, "Down Command");
-    }
-
-    public Command manualDownCommand () {
-        Command c = new RunCommand(this::motorDown, this);
-        return LoggedCommand.logCommand(c, "Manual Down Command");
-    }
-
-    public Command zeroCommand() {
-        return new RunCommand(() -> {
-            motor.set(-0.2);
-        }, this)
-                .finallyDo(()-> {
-                    zeroClimb();
-                    hookStow();
-                });
-    }
+//    private SparkMax motor;
+//    private double lastPos;
+//
+//    public ClimbSubsystem (){
+//
+//        motor = new SparkMax(cClimber, SparkLowLevel.MotorType.kBrushless);
+//        SparkBaseConfig config = new SparkMaxConfig()
+//                .apply(new EncoderConfig().positionConversionFactor(cConfigMultiplier))
+//                .apply(new ClosedLoopConfig().pid(5, 0, 0).outputRange(-0.5,1));
+//        motor.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+//        zeroClimb();
+//        hookStow();
+//        SmartDashboard.putData("5826/climb/zeroClimber", zeroCommand());
+//    }
+//
+//    @Override
+//    public void periodic() {
+//        super.periodic();
+//        SmartDashboard.putNumber("5826/climb/position", motor.getEncoder().getPosition());
+//    }
+//
+//    public void hookDown() {
+//        if(motor.getEncoder().getPosition()<=cDownPos*1.5){
+//            motor.getClosedLoopController().setSetpoint(cDownPos, SparkBase.ControlType.kPosition, ClosedLoopSlot.kSlot0, 0);
+//
+//        }else
+//            motor.getClosedLoopController().setSetpoint(cDownPos, SparkBase.ControlType.kPosition, ClosedLoopSlot.kSlot0, -6);
+//    }
+//
+//    public void hookStow() {
+//        if(motor.getEncoder().getPosition()<=cDownPos*1.5){
+//            motor.getClosedLoopController().setSetpoint(cStowPos, SparkBase.ControlType.kPosition, ClosedLoopSlot.kSlot0, 0);
+//        }else
+//            motor.getClosedLoopController().setSetpoint(cStowPos, SparkBase.ControlType.kPosition, ClosedLoopSlot.kSlot0, -6);
+//    }
+//
+//    public void hookUp() {
+//        motor.getClosedLoopController().setSetpoint(cUpPos, SparkBase.ControlType.kPosition, ClosedLoopSlot.kSlot0);
+//    }
+//
+//    public void zeroClimb () {
+//        motor.getEncoder().setPosition(0);
+//    }
+//
+//    public void motorDown () {
+//        motor.set(-0.05);
+//    }
+//
+//    public Command hookDownCommand() {
+//        Command c = new RunCommand(this::hookDown,this);
+//        return LoggedCommand.logCommand(c,"Climb Command");
+//    }
+//
+//    public Command hookUpCommand() {
+//        Command c = new RunCommand(this::hookUp, this);
+//        return LoggedCommand.logCommand(c, "Up Command");
+//    }
+//
+//    public Command stowCommand () {
+//        Command c = new RunCommand(this::hookStow, this);
+//        return LoggedCommand.logCommand(c, "Down Command");
+//    }
+//
+//    public Command manualDownCommand () {
+//        Command c = new RunCommand(this::motorDown, this);
+//        return LoggedCommand.logCommand(c, "Manual Down Command");
+//    }
+//
+//    public Command zeroCommand() {
+//        return new RunCommand(() -> {
+//            motor.set(-0.2);
+//        }, this)
+//                .finallyDo(()-> {
+//                    zeroClimb();
+//                    hookStow();
+//                });
+//    }
 }
 
 
